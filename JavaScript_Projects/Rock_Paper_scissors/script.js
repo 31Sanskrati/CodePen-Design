5111fb740c15ed57be683704775f089cf8a537d5
Sanskrati Jain <31sanskrati@gmail.com>
2021-08-04 09:02:22 +0530

    Projects

--- JavaScript_Projects/Rock_Paper_scissors/script.js
+++ JavaScript_Projects/Rock_Paper_scissors/script.js
@@ -1,11 +1,11 @@
-function game(yourChoice){
+function game(yourChoice) {
     var player, computer;
 
     player = yourChoice.id;
     console.log('Your Choice: ', player)
 
     computer = computerChoice(generateRandomNumber());
-    console.log('Computer choice: ',computer);
+    console.log('Computer choice: ', computer);
 
     results = decideWinner(player, computer);
     console.log(results);
@@ -17,21 +17,21 @@ function game(yourChoice){
 }
 
 //generate random number from 0,1,2
-function generateRandomNumber(){
-    return Math.floor(Math.random()*3);
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 3);
 }
 
 //choose for computer
-function computerChoice(number){
-    return ['rock','paper','scissor'][number];
+function computerChoice(number) {
+    return ['rock', 'paper', 'scissor'][number];
 }
 
 //cases for winning game
-function decideWinner(yourChoice, computerChoice){
+function decideWinner(yourChoice, computerChoice) {
     var rpsDatabase = {
-        'rock':{'rock':0.5, 'paper':1, 'scissor':0},
-        'paper':{'rock':0, 'paper':0.5, 'scissor':1},
-        'scissor':{'rock':1, 'paper':0, 'scissor':0.5}
+        'rock': { 'rock': 0.5, 'paper': 0, 'scissor': 1 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissor': 0 },
+        'scissor': { 'rock': 0, 'paper': 1, 'scissor': 0.5 }
     }
     //outpus is in format [0,1]
     var yourScore = rpsDatabase[yourChoice][computerChoice];
@@ -40,36 +40,40 @@ function decideWinner(yourChoice, computerChoice){
     return [yourScore, computerScore];
 }
 
-function findMessage([yourScore,computerScore]){
-    if (yourScore===1){
-        return {'text':'You Win', 'color':'green'};
+//Return an object for printing the result with particular color
+function findMessage([yourScore, computerScore]) {
+    if (yourScore === 1) {
+        return { 'text': 'You Win', 'color': '#40C4FF' };
     }
-    else if (yourScore===0){
-        return {'text':'You Lose', 'color':'red'};
+    else if (yourScore === 0) {
+        return { 'text': 'You Lose', 'color': '#FF5252' };
     }
-    else{
-        return {'text':'Draw', 'color':'yellow'};
+    else {
+        return { 'text': 'Draw', 'color': 'white' };
     }
 }
 
-function frontEnd(player, computer, message){
+//changes the frontend after choosing the hand
+function frontEnd(player, computer, message) {
     var imageDatabase = {
         'rock': document.getElementById('rock').src,
         'paper': document.getElementById('paper').src,
         'scissor': document.getElementById('scissor').src
     };
 
+    //remove all the images
     document.getElementById('rock').remove();
     document.getElementById('paper').remove();
     document.getElementById('scissor').remove();
 
+    //create the items to be displayed after choosing hand
     var humandiv = document.createElement('div');
     var computerdiv = document.createElement('div');
     var messagediv = document.createElement('div');
 
-    humandiv.innerHTML = "<img src='"+imageDatabase[player]+"' height=100 width=auto style='box-shadow: 2px 2px 5px 1px darkblue'>";
-    messagediv.innerHTML = "<h1 style='color:" +message['color'] + "; font-size:50px; padding: 20px;'>" + message['text'] + "</h1>";
-    computerdiv.innerHTML = "<img src='"+imageDatabase[computer]+"'height=100 width=auto style='box-shadow: 2px 2px 5px 1px red'>";
+    humandiv.innerHTML = "<img src='" + imageDatabase[player] + "' height=150 width=auto style='box-shadow: 3px 3px 10px 10px #40C4FF'>";
+    messagediv.innerHTML = "<h1 style='color:" + message['color'] + "; font-size:50px; padding: 20px;'>" + message['text'] + "</h1>";
+    computerdiv.innerHTML = "<img src='" + imageDatabase[computer] + "'height=150 width=auto style='box-shadow: 3px 3px 10px 10px #FF5252'>";
 
     document.getElementById('rps-div').appendChild(humandiv);
     document.getElementById('rps-div').appendChild(messagediv);
