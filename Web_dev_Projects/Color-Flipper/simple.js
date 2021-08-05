296e2de247357006a416d61cba0b956c17431578
Sanskrati Jain <31sanskrati@gmail.com>
2021-08-06 00:15:43 +0530

    Project

---
+++ Web_dev_Projects/Color-Flipper/simple.js
@@ -0,0 +1,14 @@
+const colors = ["mediumturquoise", "lightcoral", "khaki", "mediumspringgreen", "white"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
